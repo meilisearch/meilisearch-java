de9757118dbffcc885578596723af37fe753d982
AmiralBl3ndic <camille.briand@efrei.net>
2020-09-04 17:29:43 +0200

    doc(Config): add documentation for Config class

--- src/main/java/meilisearch/Config.java
+++ src/main/java/meilisearch/Config.java
@@ -1,9 +1,25 @@
 package meilisearch;
 
+/**
+ * Meilisearch configuration
+ */
 public class Config {
 	String hostUrl;
 	String apiKey;
 
+	/**
+	 * Create a configuration without an API key
+	 * @param hostUrl URL of the Meilisearch instance
+	 */
+	public Config(String hostUrl) {
+		this(hostUrl, "");
+	}
+
+	/**
+	 * Create a configuration with an API key
+	 * @param hostUrl URL of the Meilisearch instance
+	 * @param apiKey API key to pass to the header of requests sent to Meilisearch
+	 */
 	public Config(String hostUrl, String apiKey) {
 		this.hostUrl = hostUrl;
 		this.apiKey = apiKey;
