adc417814093d2cffd9f3079da6f1fc7f23dbb2b
AmiralBl3ndic <camille.briand@efrei.net>
2020-09-05 13:38:59 +0200

    doc(MeilisearchIndex): add documentation comments for MeilisearchIndex class

--- src/main/java/meilisearch/MeilisearchIndex.java
+++ src/main/java/meilisearch/MeilisearchIndex.java
@@ -4,10 +4,12 @@ import java.io.Serializable;
 
 import com.google.gson.*;
 import lombok.Getter;
+import lombok.ToString;
 
 /**
  * Meilisearch index
  */
+@ToString
 public class MeilisearchIndex implements Serializable {
 	@Getter
 	String name;
@@ -22,15 +24,19 @@ public class MeilisearchIndex implements Serializable {
 	String createdAt;
 
 	@Getter
+	@ToString.Exclude
 	String updatedAt;
 
 	@Getter
+	@ToString.Exclude
 	MeilisearchConfig config;
 
+	@ToString.Exclude
 	Documents documents;
 
 	/**
 	 * Set the Meilisearch configuration for the index
+	 *
 	 * @param config Meilisearch configuration to use
 	 */
 	void setConfig(MeilisearchConfig config) {
@@ -39,79 +45,99 @@ public class MeilisearchIndex implements Serializable {
 	}
 
 	/**
-	 * getDocument
+	 * Get a document in the index
 	 *
-	 * @param identifier
-	 * @return
-	 * @throws Exception
+	 * @param identifier Identifier of the document to get
+	 * @return Meilisearch API response
+	 * @throws Exception If something goes wrong
 	 */
 	public String getDocument(String identifier) throws Exception {
 		return this.documents.getDocument(this.uid, identifier);
 	}
 
 	/**
-	 * getDocuments
+	 * Get documents in the index
 	 *
-	 * @return
-	 * @throws Exception
+	 * @return Meilisearch API response
+	 * @throws Exception If something goes wrong
 	 */
 	public String getDocuments() throws Exception {
 		return this.documents.getDocuments(this.uid);
 	}
 
+	/**
+	 * Get documents in the index and limit the number of documents returned
+	 *
+	 * @param limits Maximum amount of documents to return
+	 * @return Meilisearch API response
+	 * @throws Exception If something goes wrong
+	 */
 	public String getDocuments(int limits) throws Exception {
 		return this.documents.getDocuments(this.uid, limits);
 	}
 
 	/**
-	 * addDocument
+	 * Add a document in the index
 	 *
-	 * @param document
-	 * @return
-	 * @throws Exception
+	 * @param document Document to add in JSON string format
+	 * @return Meilisearch API response
+	 * @throws Exception If something goes wrong
 	 */
 	public String addDocument(String document) throws Exception {
 		return this.documents.addDocument(this.uid, document);
 	}
 
 	/**
-	 * deleteDocument
+	 * Delete a document from the index
 	 *
-	 * @param identifier
-	 * @return
-	 * @throws Exception
+	 * @param identifier Identifier of the document to delete
+	 * @return Meilisearch API response
+	 * @throws Exception If something goes wrong
 	 */
 	public String deleteDocument(String identifier) throws Exception {
 		return this.documents.deleteDocument(this.uid, identifier);
 	}
 
+	/**
+	 * Delete all documents in the index
+	 *
+	 * @return Meilisearch API response
+	 * @throws Exception If something goes wrong
+	 */
 	public String deleteDocuments() throws Exception {
 		return this.documents.deleteDocuments(this.uid);
 	}
 
 	/**
-	 * search
+	 * Search documents in index
 	 *
-	 * @param q
-	 * @return
-	 * @throws Exception
+	 * @param q Query string
+	 * @return Meilisearch API response
+	 * @throws Exception If something goes wrong
 	 */
 	public String search(String q) throws Exception {
 		return this.documents.search(this.uid, q);
 	}
 
+	/**
+	 * Get an index update by its id
+	 *
+	 * @param updateId ID of the index update
+	 * @return Meilisearch API response
+	 * @throws Exception If something goes wrong
+	 */
 	public String getUpdate(int updateId) throws Exception {
 		return this.documents.getUpdate(this.uid, updateId);
 	}
 
+	/**
+	 * Get all updates of the index
+	 *
+	 * @return List of updates in the index
+	 * @throws Exception If something goes wrong
+	 */
 	public UpdateStatus[] getUpdates()  throws Exception {
 		Gson gson = new Gson();
 		return gson.fromJson(this.documents.getUpdates(this.uid), UpdateStatus[].class);
-	}
-
-	@Override
-	public String toString() {
-		// TODO: update format
-		return "Indexes:" + name + " / uid: " + uid + " / pk: " + primaryKey;
 	}
 }
