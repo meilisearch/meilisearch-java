885c0b8db93ad5e3f4ba9aec8e5d4f9d8c9b405e
AmiralBl3ndic <camille.briand@efrei.net>
2020-09-05 13:50:10 +0200

    refactor(Documents): reformat code for Documents class

--- src/main/java/meilisearch/Documents.java
+++ src/main/java/meilisearch/Documents.java
@@ -1,75 +1,78 @@
 package meilisearch;
 
+/**
+ * Wrapper around Request class to use for Meilisearch documents
+ */
 class Documents {
+	private final Request request;
 
-	Request request;
-	protected Documents (MeilisearchConfig config) {
+	protected Documents(MeilisearchConfig config) {
 		request = new Request(config);
 	}
 
-	String getDocument (String uid, String identifier) throws Exception {
+	String getDocument(String uid, String identifier) throws Exception {
 		String requestQuery = "/indexes/" + uid + "/documents/" + identifier;
 		return request.get(requestQuery);
 	}
 
-	String getDocuments (String uid) throws Exception {
+	String getDocuments(String uid) throws Exception {
 		String requestQuery = "/indexes/" + uid + "/documents";
 		return request.get(requestQuery);
 	}
 
-	String getDocuments (String uid, int limit) throws Exception {
+	String getDocuments(String uid, int limit) throws Exception {
 		String requestQuery = "/indexes/" + uid + "/documents?limit=" + limit;
 		return request.get(requestQuery);
 	}
 
-	String addDocument (String uid, String document) throws Exception {
+	String addDocument(String uid, String document) throws Exception {
 		String requestQuery = "/indexes/" + uid + "/documents";
 		return request.post(requestQuery, document);
 	}
 
-	String deleteDocument (String uid, String identifier) throws Exception {
+	String deleteDocument(String uid, String identifier) throws Exception {
 		String requestQuery = "/indexes/" + uid + "/documents/" + identifier;
 		return request.delete(requestQuery);
 	}
 
-	String deleteDocuments (String uid) throws Exception {
+	String deleteDocuments(String uid) throws Exception {
 		String requestQuery = "/indexes/" + uid + "/documents";
 		return request.delete(requestQuery);
 	}
 
-	String deleteList (String uid, int[] ids) throws Exception {
+	String deleteList(String uid, int[] ids) throws Exception {
 		// TODO
 		return "";
 	}
 
-	String search (String uid, String q) throws Exception {
+	String search(String uid, String q) throws Exception {
 		String requestQuery = "/indexes/" + uid + "/search";
 		SearchRequest sr = new SearchRequest(q);
 		return request.get(requestQuery, sr.getQuery());
 	}
 
-	String search (String uid,
-	               String q,
-	               int offset,
-	               int limit,
-	               String attributesToRetrieve,
-	               String attributesToCrop,
-	               int cropLength,
-	               String attributesToHighlight,
-	               String filters,
-	               boolean matches
+	String search(String uid,
+	              String q,
+	              int offset,
+	              int limit,
+	              String attributesToRetrieve,
+	              String attributesToCrop,
+	              int cropLength,
+	              String attributesToHighlight,
+	              String filters,
+	              boolean matches
 	) throws Exception {
 		String requestQuery = "/indexes/" + uid + "/search";
 		SearchRequest sr = new SearchRequest(q, offset, limit, attributesToRetrieve, attributesToCrop, cropLength, attributesToHighlight, filters, matches);
 		return request.get(requestQuery, sr.getQuery());
 	}
 
-	String getUpdate (String uid, int updateId) throws Exception {
+	String getUpdate(String uid, int updateId) throws Exception {
 		String requestQuery = "/indexes/" + uid + "/updates/" + updateId;
 		return request.get(requestQuery);
 	}
 
-	String getUpdates (String uid) throws Exception {
+	String getUpdates(String uid) throws Exception {
 		String requestQuery = "/indexes/" + uid + "/updates";
 		return request.get(requestQuery);
 	}
