a17c0b0994c5b01b34ad056f0a4002bca1603ad4
Samuel Jimenez <sjimenezre@gmail.com>
2020-09-28 22:16:02 +0200

    Add error message, error code and error link to the UpdateStatus Class

--- src/main/java/meilisearch/UpdateStatus.java
+++ src/main/java/meilisearch/UpdateStatus.java
@@ -1,4 +1,5 @@
 package meilisearch;
+import com.google.gson.Gson;
 
 class StatusType {
 	String name = "";
@@ -6,16 +7,18 @@ class StatusType {
 }
 
 public class UpdateStatus {
-	String status = "";
-	int updateId = 0;
-	StatusType type = null;
-	float duration = 0.0f;
-	String enqueuedAt = "";
-	String processAt = "";
+	public String status = "";
+	public int updateId = 0;
+	public StatusType type = null;
+	public float duration = 0.0f;
+	public String enqueuedAt = "";
+	public String processedAt = "";
+	public String errorCode = "";
+	public String errorType = "";
+	public String errorLink = "";
 
 	@Override
 	public String toString() {
-		// TODO: update format
-		return "Update status:" + status + " / id: " + updateId + " / process-at: " + processAt;
+		return new com.google.gson.Gson().toJson(this);
 	}
 }
