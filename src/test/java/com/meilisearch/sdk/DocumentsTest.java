227860553c2306453cf4390175bf17bed4814a20
Samuel Jimenez <sjimenezre@gmail.com>
2020-10-06 09:15:21 +0200

    Add basic Indexes and Documents tests, run integration tests in CI with docker

--- src/test/java/com/meilisearch/sdk/DocumentsTest.java
+++ src/test/java/com/meilisearch/sdk/DocumentsTest.java
@@ -1,62 +1,120 @@
 package com.meilisearch.sdk;
 
+import com.google.gson.Gson;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
 
-public class DocumentsTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-	Index meilisearchIndex;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+
+public class DocumentsTest {
+	
+	Client ms;
+	Index index;
+	Gson gson = new Gson();
+	
+	String primaryKey = "id";
 
 	@BeforeEach
-	public void initialize() {
-		Client ms = new Client(new Config("http://localhost:7700", ""));
+	public void initialize() throws Exception {
+		ms = new Client(new Config("http://localhost:7700", "masterKey"));
+	}
 
-		try {
-			// TODO: add uid of index for test
-			this.meilisearchIndex = ms.getIndex("movies");
-		} catch (Exception e) {
-
+	@AfterAll
+	static void cleanMeiliSearch()  throws Exception {
+		Client ms = new Client(new Config("http://localhost:7700", "masterKey"));
+		Index[] indexes = ms.getIndexList();
+		for (int i = 0; i < indexes.length; i++) {
+			ms.deleteIndex(indexes[i].uid);
 		}
 	}
 
+	/**
+	 * Test Add single document
+	 */
 	@Test
-	public void get() throws Exception {
-		// TODO: input identifier for test
-		System.out.println(this.meilisearchIndex.getDocument("9999"));
+	public void testAddDocumentsSingle() throws Exception {
+
+		String indexUid = "addSingleDocument";
+		ms.createIndex(indexUid);
+		this.index = ms.getIndex(indexUid);
+
+		JSONArray jsonArray = new JSONArray();
+		JSONObject jsonObject;
+
+		jsonObject  = new JSONObject()
+		.put("id", "1111")
+		.put("title", "Alice in wonderland");
+		jsonArray.put(jsonObject);
+		
+		UpdateStatus updateInfo = this.gson.fromJson(
+			index.addDocuments(jsonArray.toString()), 
+			UpdateStatus.class
+		);
+		
+		// TODO: Replace by WaitForPendingUpdate()
+		String status = "";
+		while (!status.equals("processed")){
+			UpdateStatus updateStatus = this.gson.fromJson(
+				index.getUpdate(updateInfo.getUpdateId()), 
+				UpdateStatus.class
+			);
+			status = updateStatus.getStatus();
+			Thread.sleep(20);
+		}
+		assertEquals(index.getDocuments(), jsonArray.toString());
 	}
 
+	/**
+	 * Test Add multiple documents
+	 */
 	@Test
-	public void getAll() throws Exception {
-		System.out.println(this.meilisearchIndex.getDocuments());
+	public void testAddDocumentsMultiple() throws Exception {
+
+		String indexUid = "addMultipleDocuments";
+		ms.createIndex(indexUid);
+		this.index = ms.getIndex(indexUid);
+
+		JSONArray jsonArray = new JSONArray();
+		JSONObject jsonObject;
+
+		jsonObject  = new JSONObject()
+		.put("id", "1111")
+		.put("title", "Alice in wonderland");
+		jsonArray.put(jsonObject);
+
+		jsonObject = new JSONObject()
+		.put("id", "222")
+		.put("title", "Blice in wonderland");
+		jsonArray.put(jsonObject);
+
+		jsonObject = new JSONObject()
+		.put("id", "333")
+		.put("title", "Clice in wonderland");
+		jsonArray.put(jsonObject);
+
+		UpdateStatus updateInfo = this.gson.fromJson(
+			index.addDocuments(jsonArray.toString()), 
+			UpdateStatus.class
+		);
+		
+		// TODO: Replace by WaitForPendingUpdate()
+		String status = "";
+		while (!status.equals("processed")){
+			UpdateStatus updateStatus = this.gson.fromJson(
+				index.getUpdate(updateInfo.getUpdateId()), 
+				UpdateStatus.class
+			);
+			status = updateStatus.getStatus();
+			Thread.sleep(20);
+		}
+		assertEquals(index.getDocuments(), jsonArray.toString());
 	}
 
-	@Test
-	public void add() throws Exception {
-		String testDoc = "[{\n" +
-			"      \"id\": 9999,\n" +
-			"      \"title\": \"Shazam\",\n" +
-			"      \"poster\": \"https://image.tmdb.org/t/p/w1280/xnopI5Xtky18MPhK40cZAGAOVeV.jpg\",\n" +
-			"      \"overview\": \"A boy is given the ability to become an adult superhero in times of need with a single magic word.\",\n" +
-			"      \"release_date\": \"2019-03-23\"\n" +
-			"  }]";
-		// TODO: setup test document for 'add'
-		System.out.println(this.meilisearchIndex.addDocuments(""));
-	}
-
-	@Test
-	public void delete() throws Exception {
-		// TODO: input identifier for test
-		System.out.println(this.meilisearchIndex.deleteDocument(""));
-	}
-
-	@Test
-	public void search() throws Exception {
-		System.out.println(this.meilisearchIndex.search("Batman"));
-	}
-
-	@Test
-	public void updates() throws Exception {
-		System.out.println(this.meilisearchIndex.getUpdates()[0].toString());
-	}
 }
+
