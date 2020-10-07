088712ee3f9bc646413cfb9e7908fa912dc418cc
niemannd <41839756+niemannd@users.noreply.github.com>
2020-10-07 09:52:21 +0200

    adapt MeiliSearchHttpRequest to use DefaultHttpClient

--- src/main/java/com/meilisearch/sdk/MeiliSearchHttpRequest.java
+++ src/main/java/com/meilisearch/sdk/MeiliSearchHttpRequest.java
@@ -6,7 +6,6 @@ import com.meilisearch.sdk.http.request.BasicHttpRequest;
 import com.meilisearch.sdk.http.request.HttpMethod;
 import com.meilisearch.sdk.http.response.HttpResponse;
 
-import java.util.Arrays;
 import java.util.Collections;
 
 class MeiliSearchHttpRequest {
@@ -25,27 +24,26 @@ class MeiliSearchHttpRequest {
 		return this.get(api, "");
 	}
 
-
 	String get(String api, String param) throws Exception {
 		HttpResponse<?> httpResponse = this.client.get(new BasicHttpRequest(HttpMethod.GET, api + param, Collections.emptyMap(), null));
-		return Arrays.toString(httpResponse.getContentAsBytes());
+		return new String(httpResponse.getContentAsBytes());
 	}
 
 
 	String post(String api, String params) throws Exception {
 		HttpResponse<?> httpResponse = this.client.post(new BasicHttpRequest(HttpMethod.POST, api + params, Collections.emptyMap(), null));
-		return Arrays.toString(httpResponse.getContentAsBytes());
+		return new String(httpResponse.getContentAsBytes());
 	}
 
 
 	String put(String api, String params) throws Exception {
 		HttpResponse<?> httpResponse = this.client.put(new BasicHttpRequest(HttpMethod.PUT, api + params, Collections.emptyMap(), null));
-		return Arrays.toString(httpResponse.getContentAsBytes());
+		return new String(httpResponse.getContentAsBytes());
 	}
 
 
 	String delete(String api) throws Exception {
 		HttpResponse<?> httpResponse = this.client.put(new BasicHttpRequest(HttpMethod.PUT, api, Collections.emptyMap(), null));
-		return Arrays.toString(httpResponse.getContentAsBytes());
+		return new String(httpResponse.getContentAsBytes());
 	}
 }
