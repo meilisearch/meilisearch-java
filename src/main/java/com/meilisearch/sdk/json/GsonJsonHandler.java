e1bba35ec0f073110e2385080ee18ea112b4323b
niemannd <41839756+niemannd@users.noreply.github.com>
2020-10-26 19:23:54 +0100

    centralize the serialization/deserialization logic (#36)
    * add JSON handling
    * add GsonJsonHandler
    * add JacksonJsonHandler
    * add JsonB Handler
    * add Unit Tests for Jackson and Gson JsonHandler

---
+++ src/main/java/com/meilisearch/sdk/json/GsonJsonHandler.java
@@ -0,0 +1,52 @@
+package com.meilisearch.sdk.json;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+public class GsonJsonHandler implements JsonHandler {
+	private final Gson gson;
+
+	public GsonJsonHandler() {
+		this.gson = new Gson();
+	}
+
+	public GsonJsonHandler(Gson gson) {
+		this.gson = gson;
+	}
+
+	@Override
+	public String encode(Object o) throws Exception {
+		if (o.getClass() == String.class) {
+			return (String) o;
+		}
+		try {
+			return gson.toJson(o);
+		} catch (Exception e) {
+			// todo: use dedicated exception
+			throw new RuntimeException("Error while serializing: ", e);
+		}
+	}
+
+	@Override
+	@SuppressWarnings("unchecked")
+	public <T> T decode(Object o, Class<?> targetClass, Class<?>... parameters) throws Exception {
+		if (o == null) {
+			// todo: use dedicated exception
+			throw new RuntimeException("String to deserialize is null");
+		}
+		if (targetClass == String.class) {
+			return (T) o;
+		}
+		try {
+			if (parameters == null || parameters.length == 0) {
+				return (T) gson.fromJson((String) o, targetClass);
+			} else {
+				TypeToken<?> parameterized = TypeToken.getParameterized(targetClass, parameters);
+				return gson.fromJson((String) o, parameterized.getType());
+			}
+		} catch (Exception e) {
+			// todo: use dedicated exception
+			throw new RuntimeException("Error while deserializing: ", e);
+		}
+	}
+}
---
+++ src/main/java/com/meilisearch/sdk/json/JacksonJsonHandler.java
@@ -0,0 +1,68 @@
+package com.meilisearch.sdk.json;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+
+public class JacksonJsonHandler implements JsonHandler {
+
+	private final ObjectMapper mapper;
+
+	/**
+	 * this constructor uses a default ObjectMapper with enabled 'FAIL_ON_UNKNOWN_PROPERTIES' feature.
+	 */
+	public JacksonJsonHandler() {
+		this.mapper = new ObjectMapper();
+		this.mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+	}
+
+	/**
+	 * @param mapper ObjectMapper
+	 */
+	public JacksonJsonHandler(ObjectMapper mapper) {
+		this.mapper = mapper;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public String encode(Object o) throws Exception {
+		if (o.getClass() == String.class) {
+			return (String) o;
+		}
+		try {
+			return mapper.writeValueAsString(o);
+		} catch (JsonProcessingException e) {
+			// todo: use dedicated exception
+			throw new RuntimeException("Error while serializing: ", e);
+		}
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@SuppressWarnings("unchecked")
+	@Override
+	public <T> T decode(Object o, Class<?> targetClass, Class<?>... parameters) throws Exception {
+		if (o == null) {
+			// todo: use dedicated exception
+			throw new RuntimeException("String to deserialize is null");
+		}
+		if (targetClass == String.class) {
+			return (T) o;
+		}
+		try {
+			if (parameters == null || parameters.length == 0) {
+				return (T) mapper.readValue((String) o, targetClass);
+			} else {
+				return mapper.readValue((String) o, mapper.getTypeFactory().constructParametricType(targetClass, parameters));
+			}
+		} catch (IOException e) {
+			// todo: use dedicated exception
+			throw new RuntimeException("Error while serializing: ", e);
+		}
+	}
+}
