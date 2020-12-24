a146ee28bf94246abfcf2af2f9983e45edfd64ca
luis-valdez <luiscvr15@gmail.com>
2020-12-23 20:05:18 -0700

    Add Settings class to handle settings http requests

---
+++ src/main/java/com/meilisearch/sdk/Settings.java
@@ -0,0 +1,57 @@
+package com.meilisearch.sdk;
+
+/**
+ * Settings Object for index customization
+ * Refer https://docs.meilisearch.com/references/settings.html
+ */
+public class Settings {
+	private final MeiliSearchHttpRequest meilisearchHttpRequest;
+
+	/**
+	 * Constructor for the Meilisearch Settings object
+	 *
+	 * @param config Meilisearch configuration
+	 */
+	public Settings(Config config) {
+		meilisearchHttpRequest = new MeiliSearchHttpRequest(config);
+	}
+
+	/**
+	 * Gets the settings of a given index
+	 * Refer https://docs.meilisearch.com/references/settings.html#get-settings
+	 *
+	 * @param uid Index identifier
+	 * @return settings of a given uid as String
+	 * @throws Exception
+	 */
+	public String getSettings(String uid) throws Exception {
+		return meilisearchHttpRequest.get("/indexes/" + uid + "/settings");
+	}
+
+	/**
+	 * Updates the settings of a given index
+	 * Refer https://docs.meilisearch.com/references/settings.html#update-settings
+	 *
+	 * @param uid             Index identifier
+	 * @param settingsRequest the data that contains the new settings
+	 * @return updateId
+	 * @throws Exception
+	 */
+	public String updateSettings(String uid, SettingsRequest settingsRequest) throws Exception {
+		return meilisearchHttpRequest.post("/indexes/" + uid + "/settings", settingsRequest.getUpdateQuery());
+	}
+
+	/**
+	 * Resets the settings of a given index
+	 * Refer https://docs.meilisearch.com/references/settings.html#reset-settings
+	 *
+	 * @param uid Index identifier
+	 * @return updateId
+	 * @throws Exception
+	 */
+	public String resetSettings(String uid) throws Exception {
+		return meilisearchHttpRequest.delete("/indexes/" + uid + "/settings");
+	}
+}
+
+
