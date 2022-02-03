1e0ffa6b1c4a40aab694ce0b2d7a0c2aa920f78d
alallema <amelie@meilisearch.com>
2022-02-03 13:07:58 +0100

    Fix error in build during deploy

--- src/test/java/com/meilisearch/integration/KeysTest.java
+++ src/test/java/com/meilisearch/integration/KeysTest.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-@Tag("integrationKey")
+@Tag("integration")
 public class KeysTest extends AbstractIT {
 
     @BeforeEach
