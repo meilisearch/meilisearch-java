10168ed1163fc7e8912fecde4cf3f53aae873859
Amélie <alallema@users.noreply.github.com>
2022-10-13 12:56:56 +0200

    Update src/test/java/com/meilisearch/sdk/SearchRequestTest.java
    Co-authored-by: Bruno Casali <brunoocasali@gmail.com>

--- src/test/java/com/meilisearch/sdk/SearchRequestTest.java
+++ src/test/java/com/meilisearch/sdk/SearchRequestTest.java
@@ -98,6 +98,7 @@ class SearchRequestTest {
                         true,
                         new String[] {"facets"},
                         new String[] {"sort"});
+
         assertEquals("This is a Test", classToTest.getQ());
         assertEquals(200, classToTest.getOffset());
         assertEquals(900, classToTest.getLimit());
