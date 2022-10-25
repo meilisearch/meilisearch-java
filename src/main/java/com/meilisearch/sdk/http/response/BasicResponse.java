7c8524f3da1eaaa1a93203c48e8ba9028ccd548d
alallema <amelie@meilisearch.com>
2022-10-25 11:02:42 +0200

    Clarify httpresponse content

--- src/main/java/com/meilisearch/sdk/http/response/BasicResponse.java
+++ src/main/java/com/meilisearch/sdk/http/response/BasicResponse.java
@@ -12,13 +12,11 @@ public class BasicResponse {
     public <T> HttpResponse<T> create(
             HttpResponse<T> httpResponse, Class<T> targetClass, Class<?>... parameters) {
         try {
+            T content = this.jsonHandler.decode(httpResponse.getContent(), targetClass, parameters);
             return new HttpResponse<T>(
                     httpResponse.getHeaders(),
                     httpResponse.getStatusCode(),
-                    httpResponse.getContent() == null
-                            ? null
-                            : this.jsonHandler.decode(
-                                    httpResponse.getContent(), targetClass, parameters));
+                    httpResponse.getContent() == null ? null : content);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
