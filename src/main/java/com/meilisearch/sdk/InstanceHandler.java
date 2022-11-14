aa1cc7e600d52ccf4472a13f2418b17192145601
Amélie <alallema@users.noreply.github.com>
2022-11-14 10:57:54 +0100

    Update src/main/java/com/meilisearch/sdk/InstanceHandler.java
    Co-authored-by: cvermand <33010418+bidoubiwa@users.noreply.github.com>

--- src/main/java/com/meilisearch/sdk/InstanceHandler.java
+++ src/main/java/com/meilisearch/sdk/InstanceHandler.java
@@ -5,7 +5,7 @@ import com.meilisearch.sdk.model.IndexStats;
 import com.meilisearch.sdk.model.Stats;
 
 /**
- * Wrapper around HttpClient class to get Meilisearch instance information
+ * Class providing information on the Meilisearch instance
  *
  * <p>https://docs.meilisearch.com/reference/api/keys.html
  */
