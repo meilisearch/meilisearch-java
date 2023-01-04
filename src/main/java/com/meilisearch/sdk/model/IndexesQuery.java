4b56d07d0ec6e24178615f0ea0914e073c7c8a3c
Amélie <alallema@users.noreply.github.com>
2023-01-04 12:25:02 +0100

    Update src/main/java/com/meilisearch/sdk/model/IndexesQuery.java
    Co-authored-by: cvermand <33010418+bidoubiwa@users.noreply.github.com>

--- src/main/java/com/meilisearch/sdk/model/IndexesQuery.java
+++ src/main/java/com/meilisearch/sdk/model/IndexesQuery.java
@@ -6,7 +6,7 @@ import lombok.Setter;
 import lombok.experimental.Accessors;
 
 /**
- * Data structure of a query parameter for index route
+ * Data structure of the query parameters when fetching indexes
  *
  * <p>https://docs.meilisearch.com/reference/api/indexes.html#query-parameters
  */
