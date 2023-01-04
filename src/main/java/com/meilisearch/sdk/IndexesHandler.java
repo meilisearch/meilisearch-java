c9295b1626168fe867916abdfe4b54dd8ddd284e
Amélie <alallema@users.noreply.github.com>
2023-01-04 14:34:27 +0100

    Update src/main/java/com/meilisearch/sdk/IndexesHandler.java
    Co-authored-by: cvermand <33010418+bidoubiwa@users.noreply.github.com>

--- src/main/java/com/meilisearch/sdk/IndexesHandler.java
+++ src/main/java/com/meilisearch/sdk/IndexesHandler.java
@@ -28,7 +28,7 @@ class IndexesHandler {
     /**
      * Creates an index with a unique identifier
      *
-     * @param uid Unique identifier to create the index with
+     * @param uid Unique identifier of the index
      * @return Meilisearch API response as TaskInfo
      * @throws MeilisearchException if an error occurs
      */
