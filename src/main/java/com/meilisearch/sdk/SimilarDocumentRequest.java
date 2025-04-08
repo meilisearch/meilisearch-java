22482fbf0aef9645abdc0c36884de7e59932c700
Strift <lau.cazanove@gmail.com>
2025-04-08 17:19:36 +0800

    Add tests for similar documents

--- src/main/java/com/meilisearch/sdk/SimilarDocumentRequest.java
+++ src/main/java/com/meilisearch/sdk/SimilarDocumentRequest.java
@@ -31,18 +31,17 @@ public class SimilarDocumentRequest {
 
     @Override
     public String toString() {
-        JSONObject jsonObject =
-                new JSONObject()
-                        .put("id", this.id)
-                        .put("embedder", this.embedder)
-                        .put("attributesToRetrieve", this.attributesToRetrieve)
-                        .put("offset", this.offset)
-                        .put("limit", this.limit)
-                        .put("filter", this.filter)
-                        .put("showRankingScore", this.showRankingScore)
-                        .put("showRankingScoreDetails", this.showRankingScoreDetails)
-                        .put("rankingScoreThreshold", this.rankingScoreThreshold)
-                        .put("retrieveVectors", this.retrieveVectors);
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.putOpt("id", this.id);
+        jsonObject.putOpt("embedder", this.embedder);
+        jsonObject.putOpt("attributesToRetrieve", this.attributesToRetrieve);
+        jsonObject.putOpt("offset", this.offset);
+        jsonObject.putOpt("limit", this.limit);
+        jsonObject.putOpt("filter", this.filter);
+        jsonObject.putOpt("showRankingScore", this.showRankingScore);
+        jsonObject.putOpt("showRankingScoreDetails", this.showRankingScoreDetails);
+        jsonObject.putOpt("rankingScoreThreshold", this.rankingScoreThreshold);
+        jsonObject.putOpt("retrieveVectors", this.retrieveVectors);
 
         return jsonObject.toString();
     }
