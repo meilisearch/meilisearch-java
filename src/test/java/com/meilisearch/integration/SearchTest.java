b8b3c88acf33b6a3cbb02f0bff04863dc9dcf787
Strift <lau.cazanove@gmail.com>
2025-04-08 15:52:18 +0800

    Add check for optional _vectors

--- src/test/java/com/meilisearch/integration/SearchTest.java
+++ src/test/java/com/meilisearch/integration/SearchTest.java
@@ -1266,4 +1266,45 @@ public class SearchTest extends AbstractIT {
         assertThat(results[0].getHits().size(), is(7));
         assertThat(results[1].getHits().size(), is(2));
     }
+
+    /** Test search with retrieveVectors parameter */
+    @Test
+    public void testSearchWithRetrieveVectors() throws Exception {
+        String indexUid = "testSearchWithRetrieveVectors";
+        Index index = client.index(indexUid);
+        HashMap<String, Embedder> embedders = new HashMap<>();
+        embedders.put(
+                "manual", new Embedder().setSource(EmbedderSource.USER_PROVIDED).setDimensions(3));
+
+        Settings settings = new Settings();
+        settings.setEmbedders(embedders);
+
+        index.updateSettings(settings);
+
+        TestData<Movie> testData = this.getTestData(VECTOR_MOVIES, Movie.class);
+        TaskInfo task = index.addDocuments(testData.getRaw());
+
+        index.waitForTask(task.getTaskUid());
+
+        // First search without retrieveVectors
+        SearchRequest searchRequestWithout = SearchRequest.builder().q("").build();
+        SearchResult searchResultWithout = (SearchResult) index.search(searchRequestWithout);
+
+        assertThat(searchResultWithout.getHits(), hasSize(5));
+        Map<String, Object> hitWithout = searchResultWithout.getHits().get(0);
+        assertThat(hitWithout.containsKey("_vectors"), is(false));
+
+        // Then search with retrieveVectors
+        SearchRequest searchRequestWith =
+                SearchRequest.builder().q("").retrieveVectors(true).build();
+        SearchResult searchResultWith = (SearchResult) index.search(searchRequestWith);
+
+        assertThat(searchResultWith.getHits(), hasSize(5));
+        Map<String, Object> hitWith = searchResultWith.getHits().get(0);
+        assertThat(hitWith.containsKey("_vectors"), is(true));
+
+        @SuppressWarnings("unchecked")
+        Map<String, Object> vectors = (Map<String, Object>) hitWith.get("_vectors");
+        assertThat(vectors.containsKey("manual"), is(true));
+    }
 }
