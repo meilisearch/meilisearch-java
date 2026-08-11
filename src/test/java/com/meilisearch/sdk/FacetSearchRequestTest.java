e9fb2128024a4f1544d233f3bac6643921ee4959
m0g3r <87276771+m0g3r@users.noreply.github.com>
2026-08-11 08:47:40 +0200

    Add exhaustive facet count to facet search

---
+++ src/test/java/com/meilisearch/sdk/FacetSearchRequestTest.java
@@ -0,0 +1,34 @@
+package com.meilisearch.sdk;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
+import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
+
+class FacetSearchRequestTest {
+    @Test
+    void omitsExhaustiveFacetCountByDefault() {
+        JSONObject json = new JSONObject(new FacetSearchRequest("genres").toString());
+
+        assertThat(json.has("exhaustiveFacetCount"), is(false));
+    }
+
+    @Test
+    void serializesExhaustiveFacetCount() {
+        FacetSearchRequest request = new FacetSearchRequest("genres").setExhaustiveFacetCount(true);
+
+        JSONObject json = new JSONObject(request.toString());
+
+        assertThat(json.getBoolean("exhaustiveFacetCount"), is(true));
+    }
+
+    @Test
+    void buildsWithExhaustiveFacetCount() {
+        FacetSearchRequest request =
+                FacetSearchRequest.builder().facetName("genres").exhaustiveFacetCount(true).build();
+
+        assertThat(request.getExhaustiveFacetCount(), is(equalTo(true)));
+    }
+}
